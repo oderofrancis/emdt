7574bd7acc011c2b14b7fb9dc4264273e037e82b
oderofrancis <francisodero10@gmail.com>
2024-10-26 19:50:41 +0300

    EDMT - Merging files to folder

--- build/lib/edmt/init.py
+++ build/lib/edmt/init.py
@@ -14,7 +14,7 @@ __initialized = False
 
 def init(silent=False, force=False):
     """
-    Initializes the environment with Library-specific customizations.
+    Initializes the environment with ecoscope-specific customizations.
 
     Parameters
     ----------
@@ -25,4 +25,42 @@ def init(silent=False, force=False):
 
     """
 
-    print(ASCII)
+    global __initialized
+    if __initialized and not force:
+        if not silent:
+            print("EDMT already initialized.")
+        return
+
+   
+
+    import pandas as pd
+
+    pd.options.plotting.backend = "plotly"
+
+    # Enable copy-on-write for pandas. It will be the default in pandas 3.0.
+    pd.options.mode.copy_on_write = True
+
+    from tqdm.auto import tqdm
+
+    tqdm.pandas()
+
+    import warnings
+
+    from shapely.errors import ShapelyDeprecationWarning
+
+    warnings.filterwarnings(action="ignore", category=ShapelyDeprecationWarning)
+    warnings.filterwarnings(action="ignore", category=FutureWarning)
+    warnings.filterwarnings("ignore", message=".*initial implementation of Parquet.*")
+
+    import plotly.io as pio
+
+    pio.templates.default = "seaborn"
+
+    __initialized = True
+    if not silent:
+        print(ASCII)
+
+
+
+
+__all__ = ["base"]
--- edmt/edmt.py
+++ edmt/edmt.py
@@ -1,2 +1,2 @@
-from .base.init import *
+from .base.init import init
 from .base.base import *
